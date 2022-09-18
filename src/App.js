f2793f8a4fcd4a73ff58fcb2f4b0bc2a33283763
rioto.nagano <kingdom950927@gmail.com>
2022-09-18 12:28:10 +0900

    useSelectorを利用し、listsの値を取得

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import './App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+  const lists = useSelector((state) => state.lists);
+
   return (
     <div className="App">
       <h1>ReduxでTodoリスト作成</h1>
