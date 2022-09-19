9d5b95429bfbd43e4434352da0a47ed85bdda814
rioto.nagano <kingdom950927@gmail.com>
2022-09-19 21:14:38 +0900

    Promiseを使用した非同期処理の制御

--- javascript/test.js
+++ javascript/test.js
@@ -1,16 +1,20 @@
 const getGitUsername = () => {
-  const url = 'https://api.github.com/users/Onepiece2424'
+  return new Promise((resolve, reject) => {
 
-  fetch(url).then(res => res.json())
-    .then(json => {
-      console.log('これは非同期処理成功時のメッセージです。')
-      return json.login
-    }).catch(error => {
-      console.error('これは非同期処理失敗時のメッセージです。', error)
-      return null
-    })
-  };
+    const url = 'https://api.github.com/users/Onepiece2424'
+
+    fetch(url).then(res => res.json())
+      .then(json => {
+        console.log('これは非同期処理成功時のメッセージです。')
+        return resolve(json.login)
+      }).catch(error => {
+        console.error('これは非同期処理失敗時のメッセージです。', error)
+        return reject(null)
+      })
+  })
+};
 
 const message = `GitユーザーIDは`
-const username = getGitUsername();
-console.log(message + username);
+getGitUsername().then(username => {
+  console.log(message + username);
+})
