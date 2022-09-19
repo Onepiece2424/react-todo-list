3273820fbf61cda6eaf96906722b42fab86c90f7
rioto.nagano <kingdom950927@gmail.com>
2022-09-19 21:07:25 +0900

    非同期処理の制御する前

--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,16 @@
 const getGitUsername = () => {
-  const url = `https://api.github.com/users/Onepiece2424`
-  console.log(url)
-}
+  const url = 'https://api.github.com/users/Onepiece2424'
 
-getGitUsername();
+  fetch(url).then(res => res.json())
+    .then(json => {
+      console.log('これは非同期処理成功時のメッセージです。')
+      return json.login
+    }).catch(error => {
+      console.error('これは非同期処理失敗時のメッセージです。', error)
+      return null
+    })
+  };
+
+const message = `GitユーザーIDは`
+const username = getGitUsername();
+console.log(message + username);
